6cd1c17706f1c28f69b7904e468f5ea5b4b1263b
Daniel expert <daniel.expert@outlook.com>
2023-01-05 23:42:34 -0300

    Create script.js

---
+++ ex_while/script.js
@@ -0,0 +1,48 @@
+var x = 0;
+
+while(x <= 5) {  //while estrutura de repetição com teste lógico no inicio 
+
+    console.log("Testando repetição" + x);
+
+    //incrementador
+    x++;
+}
+
+
+var arr = ['teste', 'testando', 'a', 'b'];
+var y = 0;
+
+while(y <= 3) {  //while estrutura de repetição com teste lógico no inicio 
+    
+    console.log(arr[y]);
+
+    y++;
+
+}
+
+var palavra = "Matheus";
+var i = 0;
+
+while(i <= 6) {  //while estrutura de repetição com teste lógico no inicio 
+
+    console.log(palavra[i]);
+
+    i++;
+
+
+}
+
+
+
+var c = 1;
+while(c <= 6){  //while estrutura de repetição com teste lógico no inicio 
+    console.log(`Passo ${c}`);
+    c++;
+}
+
+
+var j = 1;
+do {        //estrutura de repetição com teste logico no final
+   console.log(`Passo ${j}`)
+   j++;
+} while (j <= 10);
