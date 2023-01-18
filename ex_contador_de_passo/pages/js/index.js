48621efdef60fc96f60baf7bbc3a086b7b1d3ac1
Daniel Expert <92840891+danielexpert@users.noreply.github.com>
2023-01-17 23:53:47 -0300

    Add files via upload

---
+++ ex_contador_de_passo/pages/js/index.js
@@ -0,0 +1,30 @@
+const msg=document.querySelector('#msg')
+const inp_start=document.querySelector('#inp_start')
+const inp_end=document.querySelector('#inp_end')
+const inp_passo=document.querySelector('#inp_passo')
+const res=document.querySelector('#res')
+const btn_click=document.querySelector('#btn_click')
+
+
+function passo(params) {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function number = 0;
+while (number <= 12) {
+  console.log(number);
+  number = number + 2;
+}*/
